74c7f946842682b06883bbb8c8255d225e9caf8a
Tiago Almeida <soares343vio@hotmail.com>
2018-02-26 08:43:28 +0000

    Initial Commit

---
+++ Campeoes do Mundo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Campeoes do Mundo
+//
+//  Created by Tiago Xavier da Cunha Almeida on 26/02/18.
+//  Copyright © 2018 tiagoAlmeida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
